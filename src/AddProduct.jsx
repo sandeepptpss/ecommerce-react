b302c1389f88536e0fc6551681d1536a4da58f8f
sandeepptpss <sandeepptpss@gmail.com>
2023-05-16 22:25:42 +0530

    update product component

--- src/AddProduct.jsx
+++ src/AddProduct.jsx
@@ -1,47 +1,76 @@
 import Header from "./Header";
 import { useState } from "react";
-
-
-const Product=()=>{
-    const [title,setTitle]=useState("");
-    const [price,setPrice]=useState("");
-    const [description,setDescription] =useState("")
-    const [image,setImage] =useState("")
-
-  async function  addProduct(){
-    let productItem ={title,price,description,image}
-
-    let result = await fetch("http://127.0.0.1:8000/api/addproduct", {
+const Product = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null); 
+  async function addProduct() {
+    try {
+      let formData = new FormData();
+      formData.append("title", title);
+      formData.append("price", price);
+      formData.append("description", description);
+      formData.append("image", image);
+  
+      let result = await fetch("http://127.0.0.1:8000/api/addproduct", {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(productItem)
-    });
-    result = await result.json();
-console.warn(result);
-}
-return(
-<>
-<Header />
-        <div className="add-product-page">
+        body: formData,
+      });
+  
+      result = await result.json();
+      console.warn(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  return (
+    <>
+      <Header />
+      <div className="add-product-page">
         <div className="main-register-page  center">
-        <div className="register-page col-ms-1  col-ms-1 ">
-            <h2>Add Prodct Details</h2>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" placeholder="Enter Title"></input>
-            <br></br>
-            <input type="text"  value={price} onChange={(e)=>setPrice(e.target.value)} className="form-control" placeholder="Enter price"></input>
-            <br></br>
-            <input type="text"  value={description} onChange={(e)=>setDescription(e.target.value)} className="form-control" placeholder="Enter Description"></input>
-            <br></br>
-            <input type="file" value={image} onChange={(e)=> setImage(e.target.vlaue)}  name="image" placeholder="Choose image form-control"></input>
-            <br></br>
-            <button type="submit" onClick={addProduct} className="btn btn-primary">Add Product</button>
+          <div className="register-page col-ms-1  col-ms-1 ">
+            <h2>Add Product Details</h2>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="form-control"
+              placeholder="Enter Title"
+            />
+            <br />
+            <input
+              type="text"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              className="form-control"
+              placeholder="Enter price"
+            />
+            <br />
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="form-control"
+              placeholder="Enter Description"
+            />
+            <br />
+            <input
+              type="file"
+              onChange={(e) => setImage(e.target.files[0])} 
+              name="image"
+              className="form-control"
+            />
+            <br />
+            <button type="submit" onClick={addProduct} className="btn btn-primary">
+              Add Product
+            </button>
+          </div>
         </div>
-    </div>
-        </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
+
 export default Product;
